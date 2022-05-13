c6e3fdb806a5430230226755ea8783deb3cf462a
JS1936 <102188643+JS1936@users.noreply.github.com>
2022-05-12 23:17:43 -0700

    working on Testcases.java

--- src/test/java/Testcases.java
+++ src/test/java/Testcases.java
@@ -1,14 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals; //https://mkyong.com/gradle/junit-5-gradle-examples/
 public class Testcases {
 
     public static void main(String[] args)
     {
         System.out.println("Beginning to run testcases now");
-        access_java_main();
+
+        Assertions.assertTrue(access_java_main());
+        //Assertions.assertFalse(access_java_main());
 
     }
 
-    static void access_java_main()
+
+    public static boolean access_java_main()
     {
+        //assertEquals(5,6);
+
         System.out.println("\tTesting acccess_java_main:");
         try
         {
@@ -18,10 +26,11 @@ public class Testcases {
         catch(UnsupportedOperationException e)
         {
             System.out.println("access_java_main FAILED");
-            throw new UnsupportedOperationException("Cannot access method called 'Main.main()'");
+            return false;
+            //throw new UnsupportedOperationException("Cannot access method called 'Main.main()'");
         }
         System.out.println("access_java_main PASSED");
-        //return true;
+        return true;
 
 
 
