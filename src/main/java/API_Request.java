33721d21b5e1b260e1ca70b70ecda0158c1555dd
JS1936 <102188643+JS1936@users.noreply.github.com>
2022-12-15 03:23:12 -0800

    Resolved - HTTPS file storage (cont)

--- src/main/java/API_Request.java
+++ src/main/java/API_Request.java
@@ -30,14 +30,12 @@ public class API_Request extends API {
 
     private String player = "<playerName>";
     private HttpURLConnection connection = null;
-    //private File responseFile;// = null;
+
     private File specificRequest;
-    private URL recentMatches;// = null;// = "https://api.pubg.com/shards/steam/players?filter[playerNames]="; //needs getPlayer
+    private URL recentMatches;
     private File match_list;
     private long timestamp;
-    //private URL oneMatch;// = null;
-    //private boolean isValidRequest = false;
-    //private boolean isConnected = false;
+    //private File responseFile;// = null;
 
     public String getPlayer() { return this.player; }
     public HttpURLConnection getConnection() { return this.connection; }
@@ -100,7 +98,7 @@ public class API_Request extends API {
 
         //create "matches" subdirectory for timestamp
         this.match_list =  (Files.createDirectory(Path.of(specificRequest + "/matches")).toFile());
-        File match_telemetries_list = (Files.createDirectory(Path.of(specificRequest + "/telemeTREE!")).toFile()); //added 11/29
+        //File match_telemetries_list = (Files.createDirectory(Path.of(specificRequest + "/telemeTREE!")).toFile()); //added 11/29
         //use summary to get recent match_ids. They are then formatted to be more visually user-friendly.
         Vector<String> match_ids = getMatchIDsFromRequestPath(summary_File);
         Vector<URL> telemetry_urls = new Vector<URL>();
@@ -131,8 +129,6 @@ public class API_Request extends API {
                 File newFile2 = FileManager.makePretty(newFile);
             }
 
-
-
             telemetry_urls.add(telemetryURL);
             numMatches++;
         }
@@ -149,25 +145,16 @@ public class API_Request extends API {
 
     //Guide: https://www.tutorialspoint.com/how-can-we-read-a-json-file-in-java
     public URL getTelemetryURL(File f) throws IOException {
-        //System.out.println(">Entering getTelemetryURL");
-        //System.out.println("Attempting to get the telemetry URL for file: " + f.getName());
-
 
         String fileAsString = FileUtils.readFileToString(f);
-        //File pretty_version = FileManager.makePretty(f);
-        //System.out.println("Segment: " + fileAsString.substring(0,100));
-        //System.out.println("fileAsString: \n\n" + fileAsString);
 
         int index = fileAsString.indexOf("https://telemetry-");
-        //System.out.println("INDEX: " + index);
         String https = fileAsString.substring(index, index + 119);
         System.out.println("telemetry URL is " + https);
 
         URL telemetryURL = new URL(https);
-        System.out.println("\t\t>Attempting to connect to API. Looking at telemetryURL");
         connectToAPI_wantZIP(telemetryURL);
-        System.out.println("QUERY: " + telemetryURL.getQuery());
-        System.out.println("\t\t<Exiting  getTelemetryURL, returning the url");
+
         return telemetryURL;
     }
 
@@ -183,14 +170,11 @@ public class API_Request extends API {
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Authorization","Bearer " + this.getAPIkey());
         connection.setRequestProperty("Accept", "application/vnd.api+json");
-        //connection.setRequestProperty("Content-Type", "UTF-16" + "; charset=utf-16"); //added 12/15
-        //connection.setRequestProperty("Accept", "gzip"); //added 11/29
 
         System.out.println("Response code: " + this.connection.getResponseCode()); //expect: 200
         if(this.connection.getResponseCode() == 200) //response is valid/OK
         {
             System.out.println("Connection made. URL: " + url.toString());
-            //File newFile = new File(specificRequest + "/connect");
             //System.out.println("Preparing to get matches telemetry");
         }
         else
@@ -201,7 +185,7 @@ public class API_Request extends API {
 
     }
     public HttpURLConnection connectToAPI_wantZIP(URL url) throws IOException {
-        System.out.println("CONTENT ENCODING: " + connection.getContentEncoding());
+        //System.out.println("CONTENT ENCODING: " + connection.getContentEncoding());
         this.connection = (HttpURLConnection) url.openConnection();
 
         if(connection == null) //!isConnected
@@ -210,7 +194,6 @@ public class API_Request extends API {
             System.exit(0);
         }
 
-        //System.out.println("REQUEST PROPERTIes: " + connection.getRequestProperties().toString());//getProperty("file.encoding"));
         connection.setRequestMethod("GET");
         connection.setRequestProperty("Authorization","Bearer " + this.getAPIkey());
         connection.setRequestProperty("Accept", "application/vnd.api+json");
@@ -221,34 +204,39 @@ public class API_Request extends API {
         if(this.connection.getResponseCode() == 200) //response is valid/OK
         {
             System.out.println("Connection made. URL: " + url.toString());
-            File newFile = new File(specificRequest + "/connect");
-            File newFile2 = new File("/Users/jenniferstibbins/PubgAPI-Java-pubgEndOfNov/file");
+            //File newFile = new File(specificRequest + "/connect");
+            //File newFile2 = new File("/Users/jenniferstibbins/PubgAPI-Java-pubgEndOfNov/file");
 
 
-            //SOURCE: https://www.baeldung.com/java-curl
-
+            //SOURCE: https://www.baeldung.com/java-curl //////////////////////////////
+            //
             //Command format (curl)
             String command = "curl --compressed " + url + " -H Accept: application/vnd.api+json";
-
+            //
             //Process builder to help with curl command
             ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-
+            //
             //Process
             Process process = processBuilder.start();
-
+            //
             //Input stream
             InputStream inputStream = process.getInputStream();
-
+            //
+            //Path telemetry_Path = Path.of(specificRequest + "/matches/telemetry-for-match_id_" + match_id);
+            //            File newFile = new File(telemetry_Path.toString());//)
             File file = new File("/Users/jenniferstibbins/PubgAPI-Java-pubgEndOfNov/file");
-
+            //
             //Transfer the information
             OutputStream outputStream = new FileOutputStream(file);
             inputStream.transferTo(outputStream);
-
+            FileManager.makePretty(file); //seems to work
+            //
             //Note: this does NOT print the content
             System.out.println("output stream = " + outputStream.toString());
-
+            //
             //processBuilder.command(command);
+            ////////////////////////////////////////////////////////////////////////////////
+
         }
         else
         {
@@ -260,35 +248,6 @@ public class API_Request extends API {
 
     }
 
-    public File storeResponseToSpecifiedFileLocation(String dstPath, boolean gzip) throws IOException {
-        System.out.println("dstPath = " + dstPath);
-
-
-        InputStream inputStream = connection.getInputStream();
-        System.out.println("input stream:" + inputStream.toString());
-
-        File responseFile = new File(dstPath + ".gz"); //could just make brand new file instead of using this.responseFile (remove global variable)
-        //Note: changed .txt to .json (11/19/2022)
-        if (responseFile.exists()) {
-            System.out.println("Response file exists!");
-        } else {
-            System.out.println("Response file does not exist. Creating it now");
-
-            if (responseFile.getParentFile() != null) {
-                responseFile.getParentFile().mkdirs(); //previously order was create self, then check parent... (switched to parent, then self 11/21)
-            }
-
-            responseFile.createNewFile();
-        }
-        FileOutputStream output = new FileOutputStream(responseFile);
-        //System.out.println(output.)
-        //Gzip input stream
-        inputStream.transferTo(output);
-        //System.out.println("LOOK: " + output.g());
-        //inputStream.close();
-        output.close();
-        return responseFile;
-    }
     //Consider: returning file so that it can be custom-saved
     public File storeResponseToSpecifiedFileLocation(String dstPath) throws IOException {
         System.out.println("dstPath = " + dstPath);
