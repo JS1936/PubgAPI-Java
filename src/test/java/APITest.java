235e88c253b8e268b0f769325ce70b4b1b9d7750
JS1936 <102188643+JS1936@users.noreply.github.com>
2025-07-12 23:55:14 -0700

    Delete Bob. Comment out APITest, which creates Bob To revive Bob, run APITest using "./gradlew clean build"

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,37 +1,37 @@
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-//import API;
-//import API_Request;
+// import static org.junit.jupiter.api.Assertions.*;
+// //import API;
+// //import API_Request;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-public class APITest {
+// public class APITest {
 
-    @Test
-    void getAPIkey() throws IOException {
+//     @Test
+//     void getAPIkey() throws IOException {
 
-        try {
-            System.out.println("getAPIkey");
-            API test = new API_Request("Bob", 5); //added 5 on 07/07/25
-            String keyActual = test.getAPIkey();
-            String keyExpect = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmMjdiZDY0MC05ODk5LTAxM2EtY" +
-                    "jVmNS0wYzc0NWVlZDY1NjQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjQ5MzMzNTYxLCJwdWIiOiJibHVlaG9sZSIs" +
-                    "InRpdGxlIjoicHViZyIsImFwcCI6InB1YmdfYXBpX2xlYXJuIn0.aQZbXGdwOM8HwXLvulYN2nmUUCVgG6susMmAE6oKopY";
-            assertEquals(keyActual, keyExpect);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("Test failed with exception: " + e.getMessage());
-        }
-    }
+//         try {
+//             System.out.println("getAPIkey");
+//             API test = new API_Request("Bob", 5); //added 5 on 07/07/25
+//             String keyActual = test.getAPIkey();
+//             String keyExpect = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmMjdiZDY0MC05ODk5LTAxM2EtY" +
+//                     "jVmNS0wYzc0NWVlZDY1NjQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjQ5MzMzNTYxLCJwdWIiOiJibHVlaG9sZSIs" +
+//                     "InRpdGxlIjoicHViZyIsImFwcCI6InB1YmdfYXBpX2xlYXJuIn0.aQZbXGdwOM8HwXLvulYN2nmUUCVgG6susMmAE6oKopY";
+//             assertEquals(keyActual, keyExpect);
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//             fail("Test failed with exception: " + e.getMessage());
+//         }
+//     }
 
-    @Test
-    void getAPIplatform() throws IOException {
-        API test = new API_Request("Bob");
-        String platformActual = test.getAPIplatform();
-        String platformExpect = "steam";
+//     @Test
+//     void getAPIplatform() throws IOException {
+//         API test = new API_Request("Bob");
+//         String platformActual = test.getAPIplatform();
+//         String platformExpect = "steam";
 
-        assertEquals(platformActual, platformExpect);
-        System.out.println("getAPIPlatform test DONE");
-    }
-}
+//         assertEquals(platformActual, platformExpect);
+//         System.out.println("getAPIPlatform test DONE");
+//     }
+// }
