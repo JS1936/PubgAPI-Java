6b43fe1ece02a74a62c697601e7173f8999f8a52
JS1936 <102188643+JS1936@users.noreply.github.com>
2025-09-04 13:15:47 -0700

    Deactivate exposed API key and replace with hidden key

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -1,7 +1,9 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class API {
-    protected final String key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmMjdiZDY0MC05ODk5LTAxM2EtY" +
-            "jVmNS0wYzc0NWVlZDY1NjQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjQ5MzMzNTYxLCJwdWIiOiJibHVlaG9sZSIs" +
-            "InRpdGxlIjoicHViZyIsImFwcCI6InB1YmdfYXBpX2xlYXJuIn0.aQZbXGdwOM8HwXLvulYN2nmUUCVgG6susMmAE6oKopY";
+    private static final Dotenv dotenv = Dotenv.load();
+
+    protected final String key = Dotenv.load().get("API_KEY");
     protected final String platform = "steam";
 
     public String getAPIkey()
